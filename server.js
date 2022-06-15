ec47c4179476af5e4da5169d0cc04be654146827
mattgaither <benjamin.gaither@shipt.com>
2022-06-15 04:59:47 -0500

    linked the DB to server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const mysql = require('mysql2')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //Express middleware
-app.use(express.urlencoded({extend: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//Default response for any other request (Noy Found)
+// Default response for any other request (Not Found)
 app.use((req,res) => {
     res.status(404).end();
 });
@@ -16,3 +17,26 @@ app.listen(PORT, () => {
     console.log(`Server Running on port ${PORT}`);
 });
 
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World'
+    });
+});
+
+
+//Connect to database 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'root',
+        //Your MySQL password
+        password: 'LG1026mg',
+        database: 'election',
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
