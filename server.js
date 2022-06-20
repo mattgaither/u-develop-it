65fb71ae2466a3218589613fb8a6b78e1abd3079
mattgaither <benjamin.gaither@shipt.com>
2022-06-20 11:02:27 -0500

    added patry and changed the db tables

--- server.js
+++ server.js
@@ -28,8 +28,12 @@ const db = mysql.createConnection(
 // });
 
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
-  
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id`;
+
     db.query(sql, (err, rows) => {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -51,7 +55,12 @@ app.get('/api/candidates', (req, res) => {
 // });
 
 app.get('/api/candidate/:id', (req, res)=> {
-    const sql = `SELECT * FROM candidates Where id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row)=> {
